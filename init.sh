07c859435664be4a8fe72c4a2744cf0c62be20a9
gbox3d <jungahecho@JUNGAHEui-iMac.local>
2020-11-17 14:06:33 +0900

    ....

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
+python xml_to_csv.py -d dataset
+
 python generate_tfrecord.py --csv_input=dataset/train_labels.csv --image_dir=dataset/train --output_path=train.record --labelmap=dataset/labelmap.pbtxt
 python generate_tfrecord.py --csv_input=dataset/test_labels.csv --image_dir=dataset/test --output_path=test.record --labelmap=dataset/labelmap.pbtxt
---
+++ readme.md
@@ -0,0 +1,17 @@
+# 텐서플로 사물인식 훈련장
+
+텐서플로(tf2) 기반의 od api 용 훈련 프레임웍입니다. 
+
+## 훈련방법
+
+init.sh -> train.sh -> export.sh
+
+
+## 참고
+
+https://github.com/EdjeElectronics
+
+https://gilberttanner.com/blog/tensorflow-object-detection-with-tensorflow-2-creating-a-custom-model
+
+https://github.com/TannerGilbert/Tensorflow-Object-Detection-API-Train-Model
+
