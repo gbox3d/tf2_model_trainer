4fd0be01ba40bdcd5e05be62f72b1614c9b429c2
gbox3d <jungahecho@JUNGAHEui-iMac.local>
2020-11-17 16:12:25 +0900

    ...

--- export.sh
+++ export.sh
@@ -2,4 +2,4 @@
 python ../models/research/object_detection/exporter_main_v2.py \
     --trained_checkpoint_dir=output \
     --pipeline_config_path=dataset/ssd_efficientdet_d0_512x512_coco17_tpu-8.config \
-    --output_directory inference_graph
+    --output_directory=inference_graph
