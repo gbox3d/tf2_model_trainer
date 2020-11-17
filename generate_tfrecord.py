6f8a9a40b2eea24cb6e6b5badcb7fac038b1a3cd
gbox3d <jungahecho@JUNGAHEui-iMac.local>
2020-11-17 13:48:06 +0900

    labelmap parsing and automation

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -7,7 +7,9 @@ import io
 import pandas as pd
 
 from tensorflow.python.framework.versions import VERSION
+print(f'find tf version {VERSION}')
 if VERSION >= "2.0.0a0":
+    print('run tf1 compatible mode')
     import tensorflow.compat.v1 as tf
 else:
     import tensorflow as tf
@@ -16,24 +18,59 @@ from PIL import Image
 from object_detection.utils import dataset_util
 from collections import namedtuple, OrderedDict
 
+#od api가 설치되어야함  
+from google.protobuf import text_format
+from object_detection.protos import string_int_label_map_pb2
+
 flags = tf.app.flags
 flags.DEFINE_string('csv_input', '', 'Path to the CSV input')
 flags.DEFINE_string('output_path', '', 'Path to output TFRecord')
 flags.DEFINE_string('image_dir', '', 'Path to images')
+flags.DEFINE_string('labelmap', '', 'Path to labelmap file')
 FLAGS = flags.FLAGS
+
+_labelmap_dic = {}
+_label_box_count = 0
+
+def loadLabelMap(label_file) :
+    with open(label_file,"rt") as fd :
+        # fd = open('./res/labelmap.pbtxt',"rt")
+        _text = fd.read()
+    label_map = string_int_label_map_pb2.StringIntLabelMap()
+    try:
+        text_format.Merge(_text, label_map)
+    except text_format.ParseError:
+        label_map.ParseFromString(_text)
+
+        # print(label_map)
+        
+    for item in label_map.item:
+        print(item)
+        # print(item.name)
+        # print(item.id)
+        _labelmap_dic[item.name] = item.id
+    print('label load done!')
+
 
 
 def class_text_to_int(row_label):
-    if row_label == 'Raspberry_Pi_3':
-        return 1
-    elif row_label == 'Arduino_Nano':
-        return 2
-    elif row_label == 'ESP8266':
-        return 3
-    elif row_label == 'Heltec_ESP32_Lora':
-        return 4
-    else:
-        return None
+    global _label_box_count
+
+    _label_box_count += 1
+
+    _id = _labelmap_dic[row_label]
+    print(f'{_label_box_count} : {row_label} => {_id}')
+    return _id
+    # if row_label == 'Raspberry_Pi_3':
+    #     return 1
+    # elif row_label == 'Arduino_Nano':
+    #     return 2
+    # elif row_label == 'ESP8266':
+    #     return 3
+    # elif row_label == 'Heltec_ESP32_Lora':
+    #     return 4
+    # else:
+    #     return None
 
 
 def split(df, group):
@@ -84,6 +121,7 @@ def create_tf_example(group, path):
 
 
 def main(_):
+    loadLabelMap(FLAGS.labelmap)
     writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
     path = os.path.join(FLAGS.image_dir)
     examples = pd.read_csv(FLAGS.csv_input)
@@ -95,7 +133,6 @@ def main(_):
     writer.close()
     output_path = os.path.join(os.getcwd(), FLAGS.output_path)
     print('Successfully created the TFRecords: {}'.format(output_path))
-
 
 if __name__ == '__main__':
     tf.app.run()
