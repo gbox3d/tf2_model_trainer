a26b6e9f341e651e4263476ed1c33caccf09cc09
gbox3d <gbox3d@gmail.com>
2020-11-19 13:42:06 +0900

    ...

--- export.sh
+++ export.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python ../models/research/object_detection/exporter_main_v2.py \
+python ../../temp/models/research/object_detection/exporter_main_v2.py \
     --trained_checkpoint_dir=output \
     --pipeline_config_path=dataset/ssd_efficientdet_d0_512x512_coco17_tpu-8.config \
-    --output_directory inference_graph
+    --output_directory=inference_graph
